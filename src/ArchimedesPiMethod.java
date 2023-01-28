96e4a4501289d83a72e3af94b2ea5bb5d551d416
sfingks <gravemarks@gmail.com>
2023-01-28 14:40:10 -0800

    Completed Assignment 002 on 01/28/2023 (MK Ripley)

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,27 @@
+/**     *@author Trevor Hartman
+        * @author MK Ripley
+        *
+        * @since Version 1.0
+        * 1/28/2023
+*/
 import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        while (true) {
+            System.out.println("Please type the number of sides:");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if (n < 1) {
+                break;
+            }
+            if (n > 999999999) {
+                break;
+            }
+            double a = 180.0 / n;
+            double s = Math.sin(Math.toRadians(a));
+            double pi = n * s;
+            System.out.printf("Our Pi estimate is: %.10f%n", pi);
+        }
     }
 }
